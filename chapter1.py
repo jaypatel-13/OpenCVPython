dd438d8e61b4c3a4a669310f5308582d3bc02547
Jay Patel <jaycpatel8844@gmail.com>
2021-05-11 18:27:46 -0400

    video read and show

--- chapter1.py
+++ chapter1.py
@@ -8,4 +8,15 @@ Reading and showing an image using OpenCV
 img = cv2.imread("Resources/lenna.png")
 cv2.imshow("Output", img)
 cv2.waitKey(0)
+
+
+Create an object to read and show a video
+
+cap = cv2.VideoCapture("Resources/test_video.mp4")
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
 """
