97b8d38eed8ccf3c95226e58b672199c729fff1d
Jay Patel <jaycpatel8844@gmail.com>
2021-05-11 18:20:30 -0400

    image read and show

---
+++ chapter1.py
@@ -0,0 +1,11 @@
+import cv2
+
+print("CV Package imported")
+
+"""
+Reading and showing an image using OpenCV
+
+img = cv2.imread("Resources/lenna.png")
+cv2.imshow("Output", img)
+cv2.waitKey(0)
+"""
